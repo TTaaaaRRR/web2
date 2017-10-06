76ef247ceddffeb38839a32f34b8ee1856200b2b
Fed-3835 <rozhdestvin@fedor.ga>
2017-10-07 00:44:29 +0300

    clock is workin

--- s/dz/clock/script.js
+++ s/dz/clock/script.js
@@ -1,12 +1,11 @@
-//TODO clock
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var R = 150; // radius
+var canvas = document.getElementById("2d");
+var context = canvas.getContext("2d");
+var R = 120; // radius
 
 
 (function draw() {
-	ctx.lineWidth = 1;
-	ctx.strokeStyle = "#000000";
+	context.lineWidth = 1;
+	context.strokeStyle = "#000000";
 	window.requestAnimationFrame(draw);
 	var date = new Date();
 	var hours = date.getHours();
@@ -15,14 +14,14 @@ var R = 150; // radius
 
 	console.log(`${hours}:${minutes}:${seconds}`)
 
-	ctx.clearRect(0, 0, 302, 302);
-	ctx.beginPath();
-	ctx.arc(R + 1, R + 1, R, 0, Math.PI * 2, false); // for smoothness on edges of the canvas
-	ctx.stroke();
-	ctx.closePath();
+	context.clearRect(0, 0, 302, 302);
+	context.beginPath();
+	context.arc(R + 1, R + 1, R, 0, Math.PI * 2, false); // for smoothness on edges of the canvas
+	context.stroke();
+	context.closePath();
 
-	for(var d = 0; d < 60; d++) {
-		var angle = (d / 60) * (2 * Math.PI);
+	for(var i = 0; i < 60; i++) {
+		var angle = (i / 60) * (2 * Math.PI);
 		var px = Math.cos(angle) * R;
 		var py = -Math.sin(angle) * R;
 		var qx = 0.9 * px;
@@ -31,49 +30,51 @@ var R = 150; // radius
 		py += R + 1;
 		qx += R + 1;
 		qy += R + 1;
-
-		ctx.lineWidth = (d % 5 == 0) ? 3 : 1;
-
-		ctx.beginPath();
-		ctx.moveTo(px, py);
-		ctx.lineTo(qx, qy);
-		ctx.stroke();
-		ctx.closePath();
+        
+		context.lineWidth = (i % 5 == 0) ? 3 : 1;
+        context.strokeStyle =(i % 15 == 0) ? "#FF0000": "#000000" ;
+		context.beginPath();
+		context.moveTo(px, py);
+		context.lineTo(qx, qy);
+		context.stroke();
+		context.closePath();
 	}
 
 	secondsAngle = (seconds / 60) * (2 * Math.PI) + Math.PI;
 	minutesAngle = (minutes / 60) * (2 * Math.PI) + Math.PI;
 	hoursAngle = ((hours % 12) / 12) * (2 * Math.PI) + Math.PI;
 
-	ctx.translate(R, R);
+	context.translate(R, R);
 
-	ctx.lineWidth = 5;
-	ctx.rotate(hoursAngle);
-	ctx.beginPath();
-	ctx.moveTo(0, 0);
-	ctx.lineTo(0, 75);
-	ctx.stroke();
-	ctx.closePath();
-	ctx.rotate(-hoursAngle);
+    context.lineWidth = 5;
+    context.strokeStyle = "#C3C3C3";
+	context.rotate(hoursAngle);
+	context.beginPath();
+	context.moveTo(0, 0);
+	context.lineTo(0, 50);
+	context.stroke();
+	context.closePath();
+	context.rotate(-hoursAngle);
 
-	ctx.lineWidth = 3;
-	ctx.rotate(minutesAngle);
-	ctx.beginPath();
-	ctx.moveTo(0, 0);
-	ctx.lineTo(0, 100);
-	ctx.stroke();
-	ctx.closePath();
-	ctx.rotate(-minutesAngle);
+    context.lineWidth = 4;
+    context.strokeStyle = "#000";
+	context.rotate(minutesAngle);
+	context.beginPath();
+	context.moveTo(0, 0);
+	context.lineTo(0, 70);
+	context.stroke();
+	context.closePath();
+	context.rotate(-minutesAngle);
 
-	ctx.lineWidth = 3;
-	ctx.strokeStyle = "#FF0000";
-	ctx.rotate(secondsAngle);
-	ctx.beginPath();
-	ctx.moveTo(0, 0);
-	ctx.lineTo(0, 125);
-	ctx.stroke();
-	ctx.closePath();
-	ctx.rotate(-secondsAngle);
+	context.lineWidth = 3;
+	context.strokeStyle = "#FF0000";
+	context.rotate(secondsAngle);
+	context.beginPath();
+	context.moveTo(0, 0);
+	context.lineTo(0, 80);
+	context.stroke();
+	context.closePath();
+	context.rotate(-secondsAngle);
 
-	ctx.translate(-R, -R);
+	context.translate(-R, -R);
 })();
